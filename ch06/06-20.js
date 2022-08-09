a2b100a0f5d3ccf8f7acb6748f3f435def0b3162
김태민 <103544589+kimtaemin27@users.noreply.github.com>
2022-08-09 15:58:58 +0900

    08-09

--- ch06/06-20.js
+++ ch06/06-20.js
@@ -5,7 +5,7 @@ console.log(typeof key);
 // 객체 생성
 var obj = {};
 
-// 이름이 충돌할ㅇ 위험이 없는 유일무이한 값인 심벌을 프로퍼티 키로 사용
+// 이름이 충돌할 위험이 없는 유일무이한 값인 심벌을 프로퍼티 키로 사용
 obj[key] = 'value';
 
 console.log(obj[key]);
---
+++ ch06/06-23.js
@@ -0,0 +1,35 @@
+// undefined
+var foo;
+console.log(typeof foo);
+
+// number
+foo = 3;
+console.log(typeof foo);
+
+// string
+foo = 'Hello';
+console.log(typeof foo);
+
+// boolean
+foo = true;
+console.log(typeof foo);
+
+// object
+foo = null;
+console.log(typeof foo);
+
+// symbol
+foo = Symbol();
+console.log(typeof foo);
+
+// object
+foo = {};
+console.log(typeof foo);
+
+// object 
+foo = [];
+console.log(typeof foo);
+
+// function
+foo = function() {};
+console.log(typeof foo);
