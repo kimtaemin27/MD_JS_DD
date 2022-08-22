9ff1cdf0cafe60abc50f14af75a283996e2b8f46
김태민 <103544589+kimtaemin27@users.noreply.github.com>
2022-08-22 21:27:30 +0900

    08-22

---
+++ ch12/12-01.js
@@ -0,0 +1,7 @@
+// f(x, y) = x + y
+
+function add(x, y) {
+    return x + y;
+}
+
+console.log(add(2, 5));
---
+++ ch12/12-03.js
@@ -0,0 +1,9 @@
+// 함수 정의
+function add(x, y) {
+    return x + y;
+}
+
+// 함수 호출
+var result = add(2, 5);
+
+console.log(result);
